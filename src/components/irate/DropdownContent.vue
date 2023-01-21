6c6ac3a5b2a8d133b857c66c770693a13e411637
Vadim <snooperv@gmail.com>
2023-01-21 21:43:51 +0500

    Страница iRate (ready)

--- src/components/irate/DropdownContent.vue
+++ src/components/irate/DropdownContent.vue
@@ -18,7 +18,7 @@
           <span id="plan-max" class="max">/ 512 символов</span>
         </div>
 
-        <div class="rating" style="position: relative; top: 10px">
+        <div class="rating">
           <input
             type="radio"
             name="rates_deadlines"
@@ -63,7 +63,7 @@
             <img src="@/assets/img/rating2.png" alt="" />
           </label>
 
-          <p id="text-rate-1-1" class="text_rate"></p>
+          <p class="text_rate">Ваша оценка:</p>
         </div>
 
         <h3 class="dropdownTitle">Пути достижения целей</h3>
@@ -132,7 +132,7 @@
             <img src="@/assets/img/rating2.png" alt="" />
           </label>
 
-          <p id="text-rate-1-2" class="text_rate"></p>
+          <p class="text_rate">Ваша оценка:</p>
         </div>
 
         <h3 class="dropdownTitle">Умение работать в команде</h3>
@@ -200,7 +200,7 @@
             <img src="@/assets/img/rating2.png" alt="" />
           </label>
 
-          <p id="text-rate-1-3" class="text_rate"></p>
+          <p class="text_rate">Ваша оценка:</p>
         </div>
 
         <h3 class="dropdownTitle">Приверженность к хорошим теx.практикам</h3>
@@ -269,7 +269,7 @@
             <img src="@/assets/img/rating2.png" alt="" />
           </label>
 
-          <p id="text-rate-1-4" class="text_rate"></p>
+          <p class="text_rate">Ваша оценка:</p>
         </div>
 
         <h3 class="dropdownTitle">Уровень владения технологиями</h3>
@@ -338,7 +338,7 @@
             <img src="@/assets/img/rating2.png" alt="" />
           </label>
 
-          <p id="text-rate-1-5" class="text_rate"></p>
+          <p class="text_rate">Ваша оценка:</p>
         </div>
 
         <h3 class="dropdownTitle">Адаптивность</h3>
@@ -407,7 +407,7 @@
             <img src="@/assets/img/rating2.png" alt="" />
           </label>
 
-          <p id="text-rate-1-6" class="text_rate"></p>
+          <p class="text_rate">Ваша оценка:</p>
         </div>
 
         <input type="hidden" name="profile" :value="peerId" />
@@ -439,180 +439,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.text_rate {
-  position: absolute;
-  margin-top: 50px;
-  margin-right: -88px;
-  width: 270px;
-}
-
-b {
-  background-color: #edbd31;
-  border-radius: 50%;
-  width: 10px;
-}
-
-.rating {
-  display: flex;
-  flex-direction: row-reverse;
-  width: 200px;
-  float: right;
-  margin-right: 120px;
-  margin-top: -190px;
-  position: relative;
-}
-
-.rating input {
-  display: none;
-}
-
-.rating label {
-  position: relative;
-  cursor: pointer;
-  transition: 0.5s;
-  filter: grayscale(1);
-  margin-right: 7px;
-}
-
-.rating input:hover + label,
-.rating input:checked + label {
-  filter: grayscale(0);
-}
-
-.rating input:not(:checked) ~ label:hover,
-.rating input:not(:checked) ~ label:hover ~ label {
-  filter: grayscale(0);
-}
-
-.rating input:checked ~ label {
-  filter: grayscale(0);
-}
-
-.instruction p {
-  font-family: "Gotham Pro light", sans-serif;
-  font-weight: 1000;
-  font-size: 18px;
-  margin: 0 20px;
-}
-
-.instruction span {
-  font-weight: 1000;
-  color: #222;
-  position: relative;
-}
-
-.peers span {
-  font-size: 22px;
-  color: #222222;
-}
-
-.peers p {
-  font-weight: 1000;
-  text-align: left;
-  font-size: 14px;
-  margin-left: 30px;
-  color: #414141;
-}
-
-.dropdown-content {
-  display: none;
-  padding-top: 50px;
-  padding-bottom: 10px;
-  background-color: #ececf2;
-  width: 90%;
-  border-radius: 15px;
-  transform: translateY(-50px);
-  position: relative;
-  z-index: 1;
-}
-
-.dropdown-container {
-  margin-left: 150px;
-}
-
-.ta {
-  display: block;
-  border-radius: 15px;
-  border: 1px solid #8683f8;
-  width: 60%;
-  padding: 5px;
-  font-size: 16px;
-  resize: none;
-  margin-bottom: 30px;
-  margin-top: 15px;
-  line-height: 1.5;
-  position: relative;
-}
-
-.dropdownTitle {
-  margin: 20px 0 5px 0;
-  color: #2b2b2b;
-  font-size: 28px;
-  font-weight: 400;
-  letter-spacing: -1px;
-  text-align: left;
-}
-
-p.description {
-  position: relative;
-  font-family: "Gotham Pro light", sans-serif;
-  font-size: 18px;
-  width: 62%;
-  font-weight: 1000;
-  line-height: 18px;
-  letter-spacing: -0.04em;
-  margin: 0 0 10px 0;
-  color: #3c3c3c;
-}
-
-input[type="submit"] {
-  font-family: "Gotham Pro light", sans-serif;
-  font-weight: 1000;
-  letter-spacing: 1px;
-  background-color: #a5a4f5;
-  border: none;
-  color: white;
-  padding: 10px 7px;
-  width: 250px;
-  text-align: center;
-  font-size: 20px;
-  cursor: pointer;
-  border-radius: 25px;
-  -webkit-transition: all 0.3s ease-in-out;
-  -moz-transition: all 0.3s ease-in-out;
-  -ms-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25);
-  display: block;
-  margin-bottom: 25px;
-}
-
-input[type="submit"]:active {
-  -ms-transform: scale(0.95);
-  transform: scale(0.95);
-}
-
-.char-count {
-  text-align: right;
-  font-size: 14px;
-  position: relative;
-  top: -55px;
-  right: 41%;
-}
-
-.chars,
-.max {
-  font-size: 14px !important;
-  position: relative;
-  display: inline-block;
-  font-family: "Gotham Pro light", sans-serif;
-  font-weight: 400;
-  color: #979797;
-}
-
-.show {
-  display: block;
-}
-</style>
+<style lang="scss" src="./dropdownContent.scss" scoped></style>
