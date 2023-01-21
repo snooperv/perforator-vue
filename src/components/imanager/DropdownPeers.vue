5be0d0e953231572b2165492664264aa711a5ae8
Vadim <snooperv@gmail.com>
2023-01-21 22:46:11 +0500

    Страница "Утверждение пиров" мобилка

--- src/components/imanager/DropdownPeers.vue
+++ src/components/imanager/DropdownPeers.vue
@@ -43,6 +43,7 @@ import { openModal } from "jenesius-vue-modal";
 import PeersList from "@/components/modals/PeersList/PeersList.vue";
 import { mapState } from "vuex";
 import { API_URL } from "@/helpers/api";
+import PeersListMobile from "@/components/modals/PeersList/peersListMobile.vue";
 
 export default {
   name: "DropdownPeers",
@@ -50,10 +51,12 @@ export default {
   props: ["id"],
 
   computed: {
-    ...mapState(["workerPeers"]),
+    ...mapState(["workerPeers", "isMobile"]),
 
     openModalPeers() {
-      openModal(PeersList, { isManager: true, workerId: this.id });
+      this.isMobile
+        ? openModal(PeersListMobile, { isManager: true, workerId: this.id })
+        : openModal(PeersList, { isManager: true, workerId: this.id });
     },
   },
 
@@ -73,135 +76,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.peers span {
-  font-size: 22px;
-  color: #222222;
-}
-
-.peers p {
-  font-weight: 1000;
-  text-align: left;
-  font-size: 14px;
-  margin-left: 30px;
-  color: #414141;
-}
-
-.selected-peer-name {
-  font-size: 22px;
-  margin-top: -60px;
-  margin-left: 50px;
-  color: #2c286d;
-}
-
-.delete-peer {
-  cursor: pointer;
-  position: relative;
-  float: right;
-  margin-top: -25px;
-  margin-right: 10px;
-  font-size: 1.5em;
-  color: #d32e2e;
-}
-
-.dropdown-content {
-  padding-top: 50px;
-  background-color: #ececf2;
-  width: 95%;
-  border-radius: 15px;
-  transform: translateY(-50px);
-  position: relative;
-  z-index: 1;
-}
-
-.dropdown-container {
-  margin-left: 70px;
-  padding-bottom: 10px;
-}
-
-.accept {
-  font-family: "Gotham Pro light", sans-serif;
-  font-weight: 1000;
-  letter-spacing: 1px;
-  background-color: #a5a4f5;
-  border: none;
-  color: white;
-  padding: 10px 30px;
-  text-align: center;
-  font-size: 20px;
-  cursor: pointer;
-  border-radius: 25px;
-  transition: all 0.3s ease-in-out;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25);
-  display: block;
-  margin-top: 20px;
-  margin-bottom: 25px;
-}
-
-input[type="submit"]:active {
-  -moz-transform: scale(0.95);
-  -webkit-transform: scale(0.95);
-  -o-transform: scale(0.95);
-  -ms-transform: scale(0.95);
-  transform: scale(0.95);
-}
-
-.dropdown-description {
-  color: #848484;
-  margin-bottom: 15px;
-  font-size: 16px;
-}
-
-.selected-peer {
-  padding: 14px 10px 14px 10px;
-  width: 95%;
-  background-color: #fff;
-  border-radius: 20px;
-  margin-bottom: 10px;
-}
-
-.selected-peer-avatar {
-  /*float: left;*/
-  position: relative;
-  width: 57px;
-  height: 57px;
-  object-fit: cover;
-  left: -20px;
-  top: -15px;
-  border-radius: 50%;
-}
-
-.add-peer {
-  font-family: "Gotham Pro light", sans-serif;
-  font-weight: 1000;
-  font-size: 12px;
-  letter-spacing: 1px;
-  border: none;
-  background-color: #a5a4f5;
-  color: white;
-  cursor: pointer;
-  border-radius: 25px;
-  -webkit-transition: all 0.3s ease-in-out;
-  -moz-transition: all 0.3s ease-in-out;
-  -ms-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25);
-  padding: 7px 12px;
-  margin-top: 5px;
-  position: relative;
-  /*left: -60px;*/
-}
-
-.add-peer:active {
-  -moz-transform: scale(0.95);
-  -webkit-transform: scale(0.95);
-  -o-transform: scale(0.95);
-  -ms-transform: scale(0.95);
-  transform: scale(0.95);
-}
-
-.icon-plus {
-  color: #fff;
-}
-</style>
+<style lang="scss" src="./dropdownPeers.scss" scoped></style>
