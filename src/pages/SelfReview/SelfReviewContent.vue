48ecdd2fe36ab6fd354faf2e8db89e313de535ee
Vadim <snooperv@gmail.com>
2023-05-06 22:53:38 +0500

    Получение пользовательских вопросов в селф ревью

--- src/pages/SelfReview/SelfReviewContent.vue
+++ src/pages/SelfReview/SelfReviewContent.vue
@@ -1,32 +1,24 @@
 <template>
-  <div class="introduction" v-for="item in reviewContent">
-    <h4>{{ item.title }}</h4>
+  <div class="introduction" v-for="item in selfReview?.grades">
+    <h4>{{ item.name }}</h4>
 
     <p class="description">
       {{ item.description }}
     </p>
 
-    <div v-for="question in item.content">
-      <p class="question">
-        {{ question.grade_category_description }}
-      </p>
+    <textarea
+      maxlength="512"
+      :name="item.id"
+      v-model="item.text"
+      rows="5"
+      class="ta"
+      :disabled="!selfReview.is_draft || prId"
+    >
+    </textarea>
 
-      <!--@input="(e) => setCountSymbols(e, question.id)"-->
-
-      <textarea
-        maxlength="512"
-        :name="question.id"
-        v-model="question.comment"
-        rows="5"
-        class="ta"
-        :disabled="!selfReview.is_draft || prId"
-      >
-      </textarea>
-
-      <div class="char-count">
-        <span class="chars">{{ question.comment.length }}</span>
-        <span class="max">/ 512 символов</span>
-      </div>
+    <div class="char-count">
+      <span class="chars">{{ item.text.length }}</span>
+      <span class="max">/ 512 символов</span>
     </div>
   </div>
 </template>
@@ -52,36 +44,6 @@ export default {
 
   mounted() {
     this.$store.dispatch("getSelfReview", { id: this.prId });
-  },
-
-  watch: {
-    selfReview: {
-      handler() {
-        let smallArr = [];
-        let title = "";
-
-        this.selfReview.grades.map((grade) => {
-          if (grade.grade_category_name !== title) {
-            title &&
-              this.reviewContent.push({
-                title: title,
-                description: smallArr[0].grade_category_preview_description,
-                content: smallArr,
-              });
-            smallArr = [];
-            title = grade.grade_category_name;
-          }
-
-          smallArr.push(grade);
-        });
-
-        this.reviewContent.push({
-          title: title,
-          description: smallArr[0].grade_category_preview_description,
-          content: smallArr,
-        });
-      },
-    },
   },
 };
 </script>
